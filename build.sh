8edd4ff731570b2b98d3293157202e30f0a82fe1
RealBun1993 <1553657819@qq.com>
2024-09-18 17:13:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ python3 -m ensurepip --upgrade || true
 # 升级 pip 到最新版本
 python3 -m pip install --upgrade pip
 
-# 安装 setuptools, wheel 和 six
-python3 -m pip install setuptools wheel six
+# 安装指定版本的 setuptools 和 wheel，以及 six
+python3 -m pip install "setuptools==65.0.0" wheel six
 
 # 安装项目依赖
 python3 -m pip install -r requirements.txt
