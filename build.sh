8133213428ee4527345081b3713729fbc8a2f48b
RealBun1993 <1553657819@qq.com>
2024-09-18 17:09:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ python3 -m ensurepip --upgrade || true
 # 升级 pip 到最新版本
 python3 -m pip install --upgrade pip
 
-# 安装 setuptools 和 wheel
-python3 -m pip install setuptools wheel
+# 安装 setuptools, wheel 和 six
+python3 -m pip install setuptools wheel six
 
 # 安装项目依赖
 python3 -m pip install -r requirements.txt
