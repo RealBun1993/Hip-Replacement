c1af7cffd3716dbb956935ff56444fe24b6861e9
RealBun1993 <1553657819@qq.com>
2024-09-19 17:21:54 +0800

    Update build.sh and vercel.json

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 使用 python3 -m pip 来安装 setuptools 和 distutils
-python3 -m pip install setuptools==65.5.0
+# 更新 pip 和 setuptools
+python3 -m pip install --upgrade pip
+pip install --upgrade setuptools==65.5.0
 
-# 执行默认的构建命令
-python3 -m pip install --disable-pip-version-check --upgrade -r requirements.txt
+# 安装依赖
+pip install -r requirements.txt
