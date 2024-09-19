85393eb6bd2ca2e941d36a56837bb76bee83e9c6
RealBun1993 <1553657819@qq.com>
2024-09-19 08:46:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,11 @@ source venv/bin/activate
 # 升级 pip 到最新版本
 pip install --upgrade pip
 
-# 安装 setuptools 和 wheel 以及 six
-pip install "setuptools==58.0.0" wheel six
+# 安装 setuptools 和 wheel
+pip install setuptools wheel
+
+# 手动安装 distutils
+pip install distutils
 
 # 安装项目依赖
 pip install -r requirements.txt
-
-# 继续执行其他步骤
