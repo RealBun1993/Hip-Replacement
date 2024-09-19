70ab1ef542077800c7b41dc4a5d9ee3e9c915fc5
RealBun1993 <1553657819@qq.com>
2024-09-19 10:30:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# 确保使用 Python 3.8 创建虚拟环境
+set -ex  # 开启详细模式，输出所有命令和错误
+
+# 强制使用 Python 3.8 创建虚拟环境，如果有 python3.8 可用
 if command -v python3.8 &>/dev/null; then
     python3.8 -m venv venv
 else
@@ -17,8 +19,11 @@ pip install --upgrade pip
 # 安装 setuptools 和 wheel
 pip install setuptools==59.6.0 wheel
 
-# 安装 distutils (distlib 可作为 distutils 的替代)
+# 安装 distutils，如果需要
 pip install distlib
+
+# 安装依赖的构建工具，如 Cython 和 numpy
+pip install Cython numpy
 
 # 安装项目依赖
 pip install -r requirements.txt
