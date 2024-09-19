a5f9d972d49f51aeb2bb6f2d067936599599d725
RealBun1993 <1553657819@qq.com>
2024-09-19 10:03:29 +0800

    Update build.sh and vercel.json

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 使用指定的 Python 版本创建虚拟环境
-python3.8 -m venv venv
+# 使用默认的 Python 版本创建虚拟环境
+python -m venv venv
 
 # 激活虚拟环境
 source venv/bin/activate
@@ -9,11 +9,8 @@ source venv/bin/activate
 # 升级 pip 到最新版本
 pip install --upgrade pip
 
-# 安装 setuptools 和 wheel，指定稳定版本
+# 安装 setuptools 和 wheel
 pip install setuptools==59.6.0 wheel
-
-# 安装 distutils
-pip install python3-distutils
 
 # 安装项目依赖
 pip install -r requirements.txt
