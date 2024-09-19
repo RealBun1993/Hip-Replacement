b80ea84ba73ea88541eb44394ee3d11dec6cb0ac
RealBun1993 <1553657819@qq.com>
2024-09-19 17:11:06 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 安装 setuptools 和 distutils
-pip install setuptools==65.5.0
+# 使用 python3 -m pip 来安装 setuptools 和 distutils
+python3 -m pip install setuptools==65.5.0
 
 # 执行默认的构建命令
-pip install --disable-pip-version-check --upgrade -r requirements.txt
+python3 -m pip install --disable-pip-version-check --upgrade -r requirements.txt
