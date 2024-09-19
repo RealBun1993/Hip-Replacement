6084bad7ccbd7de0958738b738c09469de31f35b
RealBun1993 <1553657819@qq.com>
2024-09-19 09:58:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 创建虚拟环境
-python3 -m venv venv
+# 使用指定的 Python 版本创建虚拟环境
+python3.8 -m venv venv
 
 # 激活虚拟环境
 source venv/bin/activate
@@ -9,8 +9,11 @@ source venv/bin/activate
 # 升级 pip 到最新版本
 pip install --upgrade pip
 
-# 安装 setuptools 和 wheel，指定一个较旧的稳定版本
+# 安装 setuptools 和 wheel，指定稳定版本
 pip install setuptools==59.6.0 wheel
+
+# 安装 distutils
+pip install python3-distutils
 
 # 安装项目依赖
 pip install -r requirements.txt
